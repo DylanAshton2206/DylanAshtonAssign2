648926c7423f48d090af86ba84692cc3af150bc9
Dylan <Dylinger2002@gmail.com>
2022-02-12 14:16:10 -0500

    Pizza place array~ implemented next logic~ pass store value and switches activity~

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 DylanAshton n01442206
 https://github.com/DylanAshton2206/DylanAshtonAssign2
+# DylanAshtonAssign2
+App for lab 2.
+Uses what i learned in lab 1 to create a pizza ordering app.
+makes use of images, arrays, different types of buttons etc.
--- app/src/main/java/dylan/ashton/n01442206/DylanActivity.java
+++ app/src/main/java/dylan/ashton/n01442206/DylanActivity.java
@@ -7,20 +7,54 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
+
+//Dylan Ashton n01442206 RNA
 public class DylanActivity extends AppCompatActivity {
+    private ImageButton Button1;
+    private ImageButton Button2;
+    private ImageButton Button3;
+    private ImageButton Button4;
+    private Button next;
+    private String[] stores;
+    private String choice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dylan);
+        stores=getResources().getStringArray(R.array.pizza_stores);
+
+        Button1 = findViewById(R.id.pizza_pizza);    //Button 1
+        Button1.setOnClickListener(view -> choice=stores[0]);
+
+        Button2 = findViewById(R.id.pizza_73);    //Button 2
+        Button2.setOnClickListener(view -> choice=stores[1]);
+
+        Button3 = findViewById(R.id.pizza_hut);    //Button 3
+        Button3.setOnClickListener(view -> choice=stores[2]);
+
+        Button4 = findViewById(R.id.pizza_nova);    //Button 4
+        Button4.setOnClickListener(view -> choice=stores[3]);
+
+        next = findViewById(R.id.button); //next button
+        next.setOnClickListener(view -> {
+                    if (choice!=null) {
+                        Toast.makeText(DylanActivity.this, choice, Toast.LENGTH_SHORT).show();
+                        Pass();
+                    }
+            Toast.makeText(DylanActivity.this,
+                    R.string.error, Toast.LENGTH_SHORT).show();
+        });
+
+
     }
+
 
 
 
@@ -28,18 +62,12 @@ public class DylanActivity extends AppCompatActivity {
     /* Override this method to inflate the overflow menu xml file.*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Get menu inflater object.
         MenuInflater menuInflater = getMenuInflater();
-
         // Inflate the custom overflow menu
         menuInflater.inflate(R.menu.overflow_activity, menu);
-
         return true;
     }
-
-
-
     /* When user select a menu item in the overflow menu xml file, this method will be invoked.*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -66,10 +94,15 @@ public class DylanActivity extends AppCompatActivity {
     }
 
     //functions
-
     public void GoToWebPage(String yourUrl) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(yourUrl));
         startActivity(intent);
+    }
+    public void Pass(){
+        Intent i=new Intent(DylanActivity.this,OrderActivity.class);
+        String Txt=choice;
+        i.putExtra("Value",Txt);
+        startActivity(i);
     }
 }
